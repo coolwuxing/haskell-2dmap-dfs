1b689f408675f21f44964ef09a0e7d6b3f3c41f6
coolwuxing <coolwuxing@gmail.com>
2014-08-02 20:24:29 -0400

    add direction to parameters

--- treasure.hs
+++ treasure.hs
@@ -3,8 +3,12 @@ import Data.Char
 
 markMap :: [[Char]] -> Int -> Int -> Char -> [[Char]]
 markMap [] i j c = []
-markMap strList i j c = let modified = take j (strList!!i) ++ ( c : drop (j + 1) (strList!!i))
-                        in take i strList ++ (modified :drop (i + 1) strList)
+markMap strList i j c 
+    | isOut = strList
+    | (strList!!i!!j) == '-' || (strList!!i!!j) == '+' = let modified = take j (strList!!i) ++ ( c : drop (j + 1) (strList!!i))
+                  in take i strList ++ (modified :drop (i + 1) strList)
+    | otherwise = strList
+    where isOut = i < 0 || j < 0 || (i >= length strList) || (j >= length (strList!!i))
 
 deadEnd :: Int -> Int -> [String] -> Bool
 deadEnd i j [] = True
@@ -14,44 +18,27 @@ deadEnd i j map
     | j >= length (map!!i) = True
     | (map!!i!!j) == '#' = True
     | (map!!i!!j) == '+' = True
+    | (map!!i!!j) == '!' = True
     | otherwise = False
 
-depthFirst :: Int -> Int -> ([String],Int) -> ([String],Int)
-depthFirst i j ([],_) = ([],-1) 
-depthFirst i j (map,rst)  
+depthFirst :: Int -> Int -> Int -> ([String],Int) -> ([String],Int)
+depthFirst i j _ ([],_) = ([],-1) 
+depthFirst i j direction (map,rst)  
     | rst > 0 = (map,rst)
-    | (map!!i!!j) == '@' = (map, 1)
     | rst > -4 && isDead = (map, rst-1)
+    | (map!!i!!j) == '@' = (map, 1)
     | rst == -4 = let failed_map = markMap map i j '!'
-                  in (failed_map, -1)
-    | otherwise = depthFirst i j (depthFirst (i-1) j (depthFirst i (j-1) (depthFirst (i+1) j (depthFirst i (j+1) (marked_map,0)))))
+                  in (failed_map, -direction)
+    | otherwise = depthFirst i j direction (depthFirst (i-1) j 4 (depthFirst i (j-1) 3 (depthFirst (i+1) j 2 (depthFirst i (j+1) 1 (marked_map,0)))))
     where marked_map = markMap map i j '+'
           isDead = deadEnd i j map
-
---depthFirst i j map  
---    | snd rightmove == 1 = rightmove
---    | snd down == 1 = down
---    | snd left == 1 = left
---    | snd up == 1 = up
---    | otherwise = let failed_map = markMap map i j '!'
---                  in (failed_map, -1)
---    where marked_map = markMap map i j '+'
---          rightmove = (["sdfsdf","sdfsdf"],1)
---          down= (["sdfsdf","sdfsdf"],1)
---          left= (["sdfsdf","sdfsdf"],1)
---          up= (["sdfsdf","sdfsdf"],1)
---          rightmove = depthFirst i j+1 marked_map 
---          down = depthFirst i+1 j fst rightmove
---          left = depthFirst i j-1 fst down
---          up = depthFirst i-1 j fst left
 
 main = do
     strMap <- readFile "map.txt"
     putStr "This is my challenge:\n\n"
     let m = lines strMap
---    print m
     mapM_ putStrLn m
-    let (m',rst) = depthFirst 0 0 (m,0)
+    let (m',rst) = depthFirst 0 0 1 (m,0)
     if rst > 0
         then putStrLn "Woo hoo, I found the treasure :-)"
         else putStrLn "Uh oh, I could not find the treasure :-("
